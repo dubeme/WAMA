a37693edb4acb11b880226744a42498869b25ab6
dubem <dubem@dubemenyekwe.com>
2016-11-26 18:26:17 -0600

    Add more actions

--- app/wama.web/Controllers/CheckInController.cs
+++ app/wama.web/Controllers/CheckInController.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using WAMA.Core.Models.Service;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,15 +7,59 @@ namespace WAMA.Web.Controllers
 {
     public class CheckInController : Controller
     {
+        private static ICheckInService _CheckInService;
+
+        public CheckInController(ICheckInService checkInService)
+        {
+            _CheckInService = checkInService;
+        }
+
         [HttpGet]
         public IActionResult Index()
+        {
+            var ttt = new WAMA.Core.Models.DTOs.Certification();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string id)
+        {
+            if (ModelState.IsValid)
+            {
+                var loginCredential = _CheckInService.GetLogInCredential(id);
+
+                if (loginCredential == null)
+                {
+                    return RedirectToAction(nameof(PatronController.Create), nameof(PatronController).Replace(AppString.Controller, string.Empty));
+                }
+                else
+                {
+                }
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult WaiverNotSigned()
         {
             return View();
         }
 
+        [HttpGet]
+        public IActionResult WaiverSignedButCertificationExpired()
+        {
+            return View();
+        }
 
-        [HttpPost]
-        public IActionResult Index(string id)
+        [HttpGet]
+        public IActionResult Successful()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AccountSuspended()
         {
             return View();
         }
