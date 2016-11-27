6d4d3e00891665ee8a256d00aa26636eb8d1e445
dubem <dubem@dubemenyekwe.com>
2016-11-26 18:26:35 -0600

    Update logic for creating user

--- app/wama.web/Controllers/PatronController.cs
+++ app/wama.web/Controllers/PatronController.cs
@@ -1,4 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using WAMA.Core.Models.Service;
 using WAMA.Core.ViewModel.User;
 
@@ -22,18 +27,39 @@ namespace WAMA.Web.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(PatronUserAccountViewModel patron)
+        public async Task<IActionResult> Create(PatronUserAccountViewModel patron)
         {
             if (!ModelState.IsValid)
             {
-                return View();
+                ViewData[AppString.ErrorMessages] = ModelState.Values
+                    .Where(val => val.ValidationState == ModelValidationState.Invalid)
+                    .Select(val => val.Errors.FirstOrDefault().ErrorMessage);
+            }
+            else
+            {
+                try
+                {
+                    await _UserAccountService.CreateUserAsync(patron);
+
+                    return RedirectToAction(
+                        actionName: nameof(CheckInController.Index),
+                        controllerName: nameof(CheckInController).Replace(AppString.Controller, string.Empty));
+                }
+                catch (Exception ex)
+                {
+                    var errMessages = new List<string>();
+
+                    while (ex != null)
+                    {
+                        errMessages.Add(ex.Message);
+                        ex = ex.InnerException;
+                    }
+
+                    ViewData[AppString.ErrorMessages] = errMessages;
+                }
             }
 
-            _UserAccountService.CreateUser(patron);
-
-            return RedirectToAction(
-                actionName: nameof(CheckInController.Index),
-                controllerName: nameof(CheckInController).Replace(AppString.Controller, string.Empty));
+            return View();
         }
     }
 }
