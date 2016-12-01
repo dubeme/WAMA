ce2472313186827d4ca5ad5c1b4bc9cce852f10d
Dubem Enyekwe <dubeme@users.noreply.github.com>
2016-12-01 00:11:12 -0600

    Add unit test project with some tests (#78)
    * Add unit test project
    
    * Update default namespace
    
    Used this to get this test project setup
    (http://stackoverflow.com/questions/37012757/attach-unit-tests-to-asp-net-core-project)
    
    * Update test project
    
    The initial project wasn't running the tests. Hence, I dug around and
    decided to go with the xunit testing framework. It also looks like it's
    the way that the .net team at microsoft is going.
    
    https://xunit.github.io/
    
    * Add tests for some viewmodel

---
+++ app/wama.core.unit.test/ViewModel/User/PatronUserAccountViewModelTests.cs
@@ -0,0 +1,16 @@
+﻿using WAMA.Core.Models.DTOs;
+using WAMA.Core.ViewModel.User;
+using Xunit;
+
+namespace WAMAcut.ViewModel.User
+{
+    public class PatronUserAccountViewModelTests
+    {
+        [Fact]
+        public void UserAccountTypeTest()
+        {
+            var userViewModel = new PatronUserAccountViewModel();
+            Assert.Equal(UserAccountType.Patron, userViewModel.AccountType);
+        }
+    }
+}
