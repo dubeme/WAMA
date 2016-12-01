88539d6fe24f078721adb74cbb3c785ae68f7582
dubem <dubem@dubemenyekwe.com>
2016-11-30 23:54:55 -0600

    Add tests for some viewmodel

---
+++ app/wama.core.unit.test/ViewModel/User/UserAccountViewModelHelper.cs
@@ -0,0 +1,35 @@
+﻿using WAMA.Core.ViewModel.User;
+
+namespace WAMAcut.ViewModel.User
+{
+    public class UserAccountViewModelHelper
+    {
+        public const string EMAIL = "";
+        public const string FIRST_NAME = "";
+        public const string LAST_NAME = "";
+        public const string MEMBER_ID = "";
+
+        public static T UserAccountTypeTest<T>() where T : UserAccountViewModel, new()
+        {
+            return new T
+            {
+                Email = EMAIL,
+                FirstName = FIRST_NAME,
+                LastName = LAST_NAME,
+                MemberId = MEMBER_ID
+            };
+        }
+
+        public static T UserAccountTypeTest<T>(string email, string fname, string lname, string memberId)
+            where T : UserAccountViewModel, new()
+        {
+            return new T
+            {
+                Email = email,
+                FirstName = fname,
+                LastName = lname,
+                MemberId = memberId
+            };
+        }
+    }
+}
