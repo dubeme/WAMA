7cf4eb8c0217f71ed085d8430a8acb6e48f2c936
dubem <dubem@dubemenyekwe.com>
2017-01-19 16:54:41 -0600

    Add admin console tools
    * User tool
    * Inventory tool
    * Report tool
    * Clinic tool

---
+++ app/wama.web/Controllers/ReportToolController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WAMA.Web.Model;
+
+namespace WAMA.Web.Controllers
+{
+    public class ReportToolController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View($"{Constants.ADMIN_CONSOLE_REPORT_TOOL_DIRECTORY}/Index.cshtml");
+        }
+
+        public IActionResult CheckIns()
+        {
+            return View($"{Constants.ADMIN_CONSOLE_REPORT_TOOL_DIRECTORY}/CheckIns.cshtml");
+        }
+
+        public IActionResult Users()
+        {
+            return View($"{Constants.ADMIN_CONSOLE_REPORT_TOOL_DIRECTORY}/Users.cshtml");
+        }
+
+        public IActionResult Listserv()
+        {
+            return View($"{Constants.ADMIN_CONSOLE_REPORT_TOOL_DIRECTORY}/Listserv.cshtml");
+        }
+
+        public IActionResult Clinics()
+        {
+            return View($"{Constants.ADMIN_CONSOLE_REPORT_TOOL_DIRECTORY}/Clinics.cshtml");
+        }
+    }
+}
---
+++ app/wama.web/Controllers/UserToolController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WAMA.Web.Model;
+
+namespace WAMA.Web.Controllers
+{
+    public class UserToolController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View($"{Constants.ADMIN_CONSOLE_USER_TOOL_DIRECTORY}/Index.cshtml");
+        }
+
+        public IActionResult Administrators()
+        {
+            return View($"{Constants.ADMIN_CONSOLE_USER_TOOL_DIRECTORY}/Administrators.cshtml");
+        }
+
+        public IActionResult Employees()
+        {
+            return View($"{Constants.ADMIN_CONSOLE_USER_TOOL_DIRECTORY}/Employees.cshtml");
+        }
+
+        public IActionResult Managers()
+        {
+            return View($"{Constants.ADMIN_CONSOLE_USER_TOOL_DIRECTORY}/Managers.cshtml");
+        }
+
+        public IActionResult Patrons()
+        {
+            return View($"{Constants.ADMIN_CONSOLE_USER_TOOL_DIRECTORY}/Patrons.cshtml");
+        }
+    }
+}
