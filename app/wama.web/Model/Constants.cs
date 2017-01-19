5fc5d5abd8f43b3104eebc4ae7ac6a46b5e7a8ee
dubem <dubem@dubemenyekwe.com>
2017-01-19 16:51:13 -0600

    Setup admin console

---
+++ app/wama.web/Model/Constants.cs
@@ -0,0 +1,29 @@
+﻿namespace WAMA.Web.Model
+{
+    public class Constants
+    {
+        public const string ADMIN_CONSOLE_CLINIC_TOOL_DIRECTORY = "Views/AdminConsole/ClinicTool";
+        public const string ADMIN_CONSOLE_INVENTORY_TOOL_DIRECTORY = "Views/AdminConsole/InventoryTool";
+        public const string ADMIN_CONSOLE_USER_TOOL_DIRECTORY = "Views/AdminConsole/UserTool";
+        public const string ADMIN_CONSOLE_REPORT_TOOL_DIRECTORY = "Views/AdminConsole/ReportTool";
+
+        public const string ADMIN_CONSOLE_ACTIVE_TOOL = "active-admin-tool";
+
+        public const string ADMIN_CONSOLE_HOME = "admin-console-home";
+
+        public const string ADMIN_CONSOLE_USERS = "admin-console-users";
+        public const string ADMIN_CONSOLE_USERS_PATRONS = "admin-console-users-patrons";
+        public const string ADMIN_CONSOLE_USERS_EMPLOYEES = "admin-console-users-employees";
+        public const string ADMIN_CONSOLE_USERS_MANAGERS = "admin-console-users-managers";
+        public const string ADMIN_CONSOLE_USERS_ADMINISTRATORS = "admin-console-users-administrators";
+
+        public const string ADMIN_CONSOLE_REPORTS = "admin-console-reports";
+        public const string ADMIN_CONSOLE_REPORTS_CHECK_INS = "admin-console-reports-check-ins";
+        public const string ADMIN_CONSOLE_REPORTS_USERS = "admin-console-reports-users";
+        public const string ADMIN_CONSOLE_REPORTS_LISTSERV = "admin-console-reports-listserv";
+        public const string ADMIN_CONSOLE_REPORTS_CLINICS = "admin-console-reports-clinics";
+
+        public const string ADMIN_CONSOLE_INVENTORIES = "admin-console-inventories";
+        public const string ADMIN_CONSOLE_CLINICS = "admin-console-clinics";
+    }
+}
