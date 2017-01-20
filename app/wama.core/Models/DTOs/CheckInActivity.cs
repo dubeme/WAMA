9280dfe70b703951b785643623af6a41d51ea0d2
dubem <dubem@dubemenyekwe.com>
2017-01-20 03:17:00 -0600

    Add property to track if logged in
    THis field is of no use at the moment. Since when some checks in,
    they don't check out when they're done. Hence, this might just be handy
    for employees. You can see who is on clock.

--- app/wama.core/Models/DTOs/CheckInActivity.cs
+++ app/wama.core/Models/DTOs/CheckInActivity.cs
@@ -5,8 +5,11 @@ namespace WAMA.Core.Models.DTOs
     public class CheckInActivity : TableRow
     {
         public string MemberId { get; set; }
+
         public DateTimeOffset CheckInDateTime { get; set; }
 
         public virtual UserAccount Member { get; set; }
+
+        public bool IsCheckedIn { get; set; }
     }
 }
