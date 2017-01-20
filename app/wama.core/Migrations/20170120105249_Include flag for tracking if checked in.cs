7ff088af9fd11582e3ebcf9cc04dfa605206bc37
dubem <dubem@dubemenyekwe.com>
2017-01-20 04:58:27 -0600

    Update database to reflect changes in DTOs

---
+++ app/wama.core/Migrations/20170120105249_Include flag for tracking if checked in.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WAMA.Core.Migrations
+{
+    public partial class Includeflagfortrackingifcheckedin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCheckedIn",
+                table: "CheckInActivities",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCheckedIn",
+                table: "CheckInActivities");
+        }
+    }
+}
