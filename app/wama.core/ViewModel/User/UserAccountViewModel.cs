2a6cdcb4bf166d8122ed861d9a7fdb0013731824
israelCyabukombe <israel.cyabukombe@jacks.sdstate.edu>
2017-01-22 18:44:13 -0600

    Add the Certification to UserAccountViewModel.cs

--- app/wama.core/ViewModel/User/UserAccountViewModel.cs
+++ app/wama.core/ViewModel/User/UserAccountViewModel.cs
@@ -19,11 +19,15 @@ namespace WAMA.Core.ViewModel.User
         [Display(Name = "Gender")]
         public string Gender { get; set; }
         
+        [Required]
+        [Display(Name = "Certification")]
+        public string Certification { get; set; }
+        
         [Required]        
         [EmailAddress]
         [Display(Name = "Email Address")]
         public string Email { get; set; }
-
+        
         [Required]
         [Display(Name = "Member Id")]
         public string MemberId { get; set; }
