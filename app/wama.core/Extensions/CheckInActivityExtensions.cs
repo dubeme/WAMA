d80495169a8b049aa45c2f153edfe12bba1903bb
dubem <dubem@dubemenyekwe.com>
2017-01-30 01:12:24 -0600

    Add more viewmodels and extension methods

---
+++ app/wama.core/Extensions/CheckInActivityExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WAMA.Core.Models.DTOs;
+using WAMA.Core.ViewModel;
+
+namespace WAMA.Core.Extensions
+{
+    public static class CheckInActivityExtensions
+    {
+        public static CheckInActivity ToDTO(this CheckInActivityViewModel viewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static CheckInActivityViewModel ToViewModel(this CheckInActivity dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ app/wama.core/Extensions/LogInCredentialExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using WAMA.Core.Models.DTOs;
+using WAMA.Core.ViewModel;
+
+namespace WAMA.Core.Extensions
+{
+    public static class LogInCredentialExtensions
+    {
+        public static LogInCredential ToDTO(this LogInCredentialViewModel viewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static LogInCredentialViewModel ToViewModel(this LogInCredential dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
