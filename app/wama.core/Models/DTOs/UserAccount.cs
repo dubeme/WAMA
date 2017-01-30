0c2b25d26198981ee86778ee8648ce4aa7dd5a35
Dubem Enyekwe <dubeme@users.noreply.github.com>
2017-01-30 12:02:34 -0600

    Update DTOs as per new features (#109) (#110)
    * Update to async version (issue #100)
    
    * Update logic for delaying account creation
    
    * Fix null reference bug (user doesnt exist)
    
    * Persist member ID on error while creating account #102
    
    * Display error while creating account with existing member ID #103
    
    * Remove frontend folder (AGAIN!!!)
    
    * Add more viewmodels and extension methods
    
    * Update DTOs as per new requirements
    
    * Update ViewModels as per new requirements
    
    * Update service classes as per new requirements
    
    * Just another refactor
    
    Update more logic, especially in the extension methods. Also clean up
    the services

--- app/wama.core/Models/DTOs/UserAccount.cs
+++ app/wama.core/Models/DTOs/UserAccount.cs
@@ -4,11 +4,21 @@ namespace WAMA.Core.Models.DTOs
 {
     public class UserAccount : TableRow
     {
+
+        public UserAccountType AccountType { get; set; }
+
         public string MemberId { get; set; }
 
         public string FirstName { get; set; }
+
         public string LastName { get; set; }
-        public UserAccountType AccountType { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string Gender { get; set; }
+
+        public bool HasBeenApproved { get; set; }
+
         public bool IsSuspended { get; set; }
 
         public virtual LogInCredential LogInCredential { get; set; }
