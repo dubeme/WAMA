78d315585db18e3323310ff66e1ca4416eb9d7de
dubem <dubem@dubemenyekwe.com>
2017-01-30 01:27:45 -0600

    Update DTOs as per new requirements

--- app/wama.core/Migrations/20170130072618_add more fields as per new requirements.Designer.cs
+++ app/wama.core/Migrations/20170130072618_add more fields as per new requirements.Designer.cs
@@ -9,9 +9,10 @@ using WAMA.Core.Models.DTOs;
 namespace WAMA.Core.Migrations
 {
     [DbContext(typeof(WamaDbContext))]
-    partial class WamaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170130072618_add more fields as per new requirements")]
+    partial class addmorefieldsaspernewrequirements
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.0-rtm-22752")
--- app/wama.core/Models/DTOs/LoginCredential.cs
+++ app/wama.core/Models/DTOs/LoginCredential.cs
@@ -3,7 +3,9 @@
     public class LogInCredential : TableRow
     {
         public string MemberId { get; set; }
+
         public string Password { get; set; }
+
         public bool RequiresPassword { get; set; }
 
         public virtual UserAccount Member { get; set; }
