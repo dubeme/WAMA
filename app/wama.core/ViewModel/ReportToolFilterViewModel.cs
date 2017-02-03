8a0b62cb938343cdde4bf9c882f89fe5049c6dda
dubem <dubem@dubemenyekwe.com>
2017-02-02 19:26:22 -0600

    Add new features
    * Generate check-in reports
    * Download check-in reports as CSV

---
+++ app/wama.core/ViewModel/ReportToolFilterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WAMA.Core.ViewModel
+{
+    public class ReportToolFilterViewModel
+    {
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Start Date")]
+        public DateTimeOffset StartDate { get; set; } = DateTimeOffset.MinValue;
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "End Date")]
+        public DateTimeOffset EndDate { get; set; } = DateTimeOffset.MaxValue;
+
+        public string ActiveTool { get; set; }
+    }
+}
