ea9ed6ced6cdb0d3df749df82c9a3960b7948cd3
dubem <dubem@dubemenyekwe.com>
2017-02-18 22:18:50 -0600

    Update DTO property

---
+++ app/wama.core/Migrations/20170219032549_updated property name.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WAMA.Core.Migrations
+{
+    public partial class updatedpropertyname : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InstitutionAffliation",
+                table: "UserAccounts");
+
+            migrationBuilder.AddColumn<int>(
+                name: "InstitutionAffiliation",
+                table: "UserAccounts",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InstitutionAffiliation",
+                table: "UserAccounts");
+
+            migrationBuilder.AddColumn<int>(
+                name: "InstitutionAffliation",
+                table: "UserAccounts",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
