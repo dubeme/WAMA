420a5ce221859be550fd3d54085613d0fc65b43e
dubem <dubem@dubemenyekwe.com>
2017-02-18 23:57:42 -0600

    Add custom model binder for UserAccountViewModel
    This should make it possible to accept the abstract class
    "UserAccountViewModel" as a parameter to an Action.
    
    Prior, you got an exception, since an abstract class cant be
    instantiated

---
+++ app/wama.web/Model/WamaModelBinderProvider.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using WAMA.Core.ViewModel.User;
+
+namespace WAMA.Web.Model
+{
+    public class WamaModelBinderProvider : IModelBinderProvider
+    {
+        public IModelBinder GetBinder(ModelBinderProviderContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (Equals(typeof(UserAccountViewModel), context.Metadata.ModelType))
+            {
+                return new UserAccountViewModelBinder();
+            }
+
+            return null;
+        }
+    }
+}
