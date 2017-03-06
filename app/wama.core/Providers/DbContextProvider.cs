eda4819bfac8f531417cba24c17237dc3194182a
dubem <dubem@dubemenyekwe.com>
2017-03-05 21:23:33 -0600

    Document Providers

--- app/wama.core/Providers/DbContextProvider.cs
+++ app/wama.core/Providers/DbContextProvider.cs
@@ -4,13 +4,26 @@ using WAMA.Core.Models.Provider;
 
 namespace WAMA.Core.Providers
 {
+    /// <summary>
+    /// Represents DbContextProvider
+    /// </summary>
+    /// <seealso cref="WAMA.Core.Models.Provider.IDbContextProvider" />
     public class DbContextProvider : IDbContextProvider
     {
+        /// <summary>
+        /// Gets a <see cref="WamaDbContext" />.
+        /// </summary>
+        /// <returns></returns>
         public WamaDbContext GetWamaDbContext()
         {
             return new WamaDbContext();
         }
 
+        /// <summary>
+        /// Gets a <see cref="WamaDbContext" />.
+        /// </summary>
+        /// <param name="dbOption">The database option.</param>
+        /// <returns></returns>
         public WamaDbContext GetWamaDbContext(DbContextOptions dbOption)
         {
             return new WamaDbContext(dbOption);
