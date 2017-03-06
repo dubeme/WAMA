649a6179122ac9202fecff1e84e7abe7b9496c19
dubem <dubem@dubemenyekwe.com>
2017-03-05 20:54:39 -0600

    Document ViewModels

--- app/wama.core/ViewModel/ListservViewModel.cs
+++ app/wama.core/ViewModel/ListservViewModel.cs
@@ -5,22 +5,50 @@ using WAMA.Core.Models.DTOs;
 
 namespace WAMA.Core.ViewModel
 {
+    /// <summary>
+    /// Represents ListservViewModel
+    /// </summary>
+    /// <seealso cref="WAMA.Core.Models.Contracts.ISerializableToCSV" />
     public class ListservViewModel : ISerializableToCSV
     {
+        /// <summary>
+        /// Gets the type of the account.
+        /// </summary>
+        /// <value>
+        /// The type of the account.
+        /// </value>
         public virtual UserAccountType AccountType { get; }
 
+        /// <summary>
+        /// Gets or sets the first name of this ListservViewModel.
+        /// </summary>
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
+        /// <summary>
+        /// Gets or sets the last name of this ListservViewModel.
+        /// </summary>
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
+        /// <summary>
+        /// Gets or sets the name of the middle.
+        /// </summary>
+        /// <value>
+        /// The name of the middle.
+        /// </value>
         [Display(Name = "Middle Name")]
         public string MiddleName { get; set; }
 
+        /// <summary>
+        /// Gets or sets the email of this ListservViewModel.
+        /// </summary>
         [Display(Name = "Email Address")]
         public string Email { get; set; }
 
+        /// <summary>
+        /// Gets the headers.
+        /// </summary>
         public IEnumerable<string> Headers
         {
             get
@@ -34,6 +62,9 @@ namespace WAMA.Core.ViewModel
             }
         }
 
+        /// <summary>
+        /// Gets the CSV string.
+        /// </summary>
         public IEnumerable<string> CSVString
         {
             get
--- app/wama.core/ViewModel/User/PatronUserAccountViewModel.cs
+++ app/wama.core/ViewModel/User/PatronUserAccountViewModel.cs
@@ -2,8 +2,15 @@
 
 namespace WAMA.Core.ViewModel.User
 {
+    /// <summary>
+    /// Represents PatronUserAccountViewModel
+    /// </summary>
+    /// <seealso cref="WAMA.Core.ViewModel.User.UserAccountViewModel" />
     public class PatronUserAccountViewModel : UserAccountViewModel
     {
+        /// <summary>
+        /// Gets the type of the account.
+        /// </summary>
         public override UserAccountType AccountType { get; } = UserAccountType.Patron;
     }
 }
