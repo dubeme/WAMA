4190d2ba74f7c56a8954c83da37ef71fb27b5d45
dubem <dubem@dubemenyekwe.com>
2017-03-14 10:13:18 -0500

    Migrate DB changes

---
+++ app/wama.core/Migrations/20170310220100_RenamedPasswordColumn.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WAMA.Core.Migrations
+{
+    public partial class RenamedPasswordColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Password",
+                table: "LogInCredentials",
+                newName: "HashedPassword");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "HashedPassword",
+                table: "LogInCredentials",
+                newName: "Password");
+        }
+    }
+}
