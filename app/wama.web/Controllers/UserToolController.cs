a1e5335712b5ce38c47bbf78eb5f0e2993e205be
AboodV2 <alkhabbaz.abdullah@gmail.com>
2017-04-08 19:13:19 -0500

    Fix bug with view discovery not find the view

--- app/wama.web/Controllers/UserToolController.cs
+++ app/wama.web/Controllers/UserToolController.cs
@@ -230,7 +230,7 @@ namespace WAMA.Web.Controllers
             }
 
             ViewBag.MemberId = Certification.MemberId;
-            return View(Certification);
+            return View($"{Constants.ADMIN_CONSOLE_USER_TOOL_DIRECTORY}/AddCertification.cshtml",Certification);
         }
 
         public async Task<IActionResult> ViewCertifications(string memberId)
