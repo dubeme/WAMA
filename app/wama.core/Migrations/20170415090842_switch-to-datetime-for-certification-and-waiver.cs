2b112ea968dd71be54e20aa22f6fe3e2b0e4b188
dubem <dubem@dubemenyekwe.com>
2017-04-15 04:25:40 -0500

    Switch to DateTime
    For the sake of the state of this project, also looking at the problem
    from a higher perspective. The updated firlds makes sense, to only
    track the date.
    
    Yes, DateTimeOffset will give more details. But for now, this will do

---
+++ app/wama.core/Migrations/20170415090842_switch-to-datetime-for-certification-and-waiver.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WAMA.Core.Migrations
+{
+    public partial class switchtodatetimeforcertificationandwaiver : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "SignedOn",
+                table: "Waivers",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ExpiresOn",
+                table: "Certifications",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CertifiedOn",
+                table: "Certifications",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "SignedOn",
+                table: "Waivers",
+                nullable: false,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "ExpiresOn",
+                table: "Certifications",
+                nullable: false,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CertifiedOn",
+                table: "Certifications",
+                nullable: false,
+                oldClrType: typeof(DateTime));
+        }
+    }
+}
